a640988cb878fd4f079c930191d786a52fd3a0e2
Ayush74-w <royayush241@outlook.com>
2024-06-10 22:38:41 +0530

    adding a new project

---
+++ src/employee/managment/system/splash.java
@@ -0,0 +1,35 @@
+package employee.managment.system;
+
+import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.*;
+
+public class splash extends JFrame {
+    splash(){
+        
+ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Assets/front.gif"));
+Image i2=i1.getImage().getScaledInstance(1200,650,Image.SCALE_DEFAULT);
+ImageIcon i3=new ImageIcon(i2);
+JLabel image=new JLabel(i3);
+image.setBounds(0,0,1200,650);
+add(image);
+
+
+setSize(1200,650);
+setLocation(200,50);
+setLayout(null);
+setVisible(true);
+try{
+    Thread.sleep(4000);
+    setVisible(false);
+    new login();
+}catch(Exception e){
+    e.printStackTrace();
+}
+    }
+
+    public static void main(String args[]) {
+        new splash();
+    }
+}
