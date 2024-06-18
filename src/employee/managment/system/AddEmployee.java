ce079998e929172ebe8020aec96922a32fc0d984
Ayush74-w <royayush241@outlook.com>
2024-06-18 22:19:21 +0530

    added new file to add employee details

---
+++ src/employee/managment/system/AddEmployee.java
@@ -0,0 +1,27 @@
+package employee.managment.system;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AddEmployee extends JFrame {
+
+    AddEmployee(){
+        getContentPane().setBackground(new Color(163,255,188));
+
+
+        JLabel heading=new JLabel("Add Employee Detail");
+        heading.setBounds(320,30,500,50);
+        heading.setFont(new Font("serif",Font.BOLD,25));
+        add(heading);
+        setSize(900,700);
+        setLayout(null);
+        setLocation(300,50);
+        setVisible(true);
+
+
+    }
+        public static void main(String args[]){
+new AddEmployee();
+        }
+
+}
