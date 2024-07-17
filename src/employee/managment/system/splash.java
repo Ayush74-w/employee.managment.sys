2f6c9e5e153f661dfb8569d60004927a1900ef25
Ayush74-w <royayush241@outlook.com>
2024-07-18 00:14:36 +0530

    minor changes done

--- src/employee/managment/system/splash.java
+++ src/employee/managment/system/splash.java
@@ -8,7 +8,7 @@ import java.awt.*;
 public class splash extends JFrame {
     splash(){
         
-ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Assets/front.gif"));
+ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Assets/source.gif"));
 Image i2=i1.getImage().getScaledInstance(1200,650,Image.SCALE_DEFAULT);
 ImageIcon i3=new ImageIcon(i2);
 JLabel image=new JLabel(i3);
